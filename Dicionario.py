d0d646febf307d86987a5c6d5b0fca02d1f6de5f
PedroSimbera <pedrosimbera@hotmail.com>
2023-05-16 19:31:10 -0300

    sobre dicionario em python

---
+++ Dicionario.py
@@ -0,0 +1,15 @@
+# Um dicionário em python é uma lista de valores com índices mais amigáveis. 
+# São estruturas de dados que mapeiam chaves a valores.
+
+# Utilizam '{ }' - para criar dicionários.
+#Também são usadas em conjuntos (sets), que são coleções não ordenadas de elementos únicos.
+# Exemplo dicionario = {'chave1': valor1, 'chave2': valor2}
+
+# Assim, o dicionário atribui nomes a cada valor, o nome é o índice.
+
+# Na hora que for chamar para impressão por exemplo, temos como opção os nomes dos índices:
+
+# dicionario = {"nome": "pedro", "sobrenome": "simbera"}
+# print(dicionario["nome"],lista["sobrenome"])
+
+# Muito utilizado em data science e analytics.
