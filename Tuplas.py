ae8cc14f1c066ad3d911ae4043979d4d6bbf04ea
PedroSimbera <pedrosimbera@hotmail.com>
2023-05-16 19:17:55 -0300

    adicionando arquiva de tupla e conceituando

---
+++ Tuplas.py
@@ -0,0 +1,11 @@
+# A diferença de tupla para lista é que a primeira é imutável, se for determinada não poderia tirar elemento,
+# adicionar elemento, ou alterar.
+# Lista são utilizadas com '[ ]' e tuplas com '( )'.
+# Bom para estrutura constante que não irá ser alterada.
+
+
+# Com tuplas, igualmente em listas, é possível verificar a existência de determinados elementos dentro de seu conjunto
+# Com o 'in tupla' 
+
+# If x in tupla:
+
